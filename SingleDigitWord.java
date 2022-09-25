c195a4a878da5a5ec5e8d7feb870ecdd75cd36c6
vaidya0303 <vaidya0304@gmail.com>
2022-09-25 14:13:49 +0530

    single digit into word display

--- SingleDigitWord.java
+++ SingleDigitWord.java
@@ -1,7 +1,7 @@
 
 	import java.util.Scanner;
 	
-	class SingleDigitWord {
+	class SingleDigitWord   {
 		
 		public static void main(String[] args) {
 			System.out.print("Enter your Number here = ");
